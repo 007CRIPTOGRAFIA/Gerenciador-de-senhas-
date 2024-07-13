ede2372a581ac1e5be145db838b71bbe11781fa6
007CRIPTOGRAFIA <122584002+007CRIPTOGRAFIA@users.noreply.github.com>
2024-07-13 20:04:14 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "Funcoes.h"
+using namespace std;
+
+int main()
+{
+    Funcoes f;
+    cout << "Boas vindas ao gerenciador de senhas!" << endl;
+    int leitura;
+    while (true)
+    {
+        Senha s;
+        int leitura = f.imprimirMenu();
+        switch (leitura)
+        {
+        case 0:
+            cout << "Obrigada por usar o gerenciador de senhas!" << endl;
+            return 0;
+            break;
+        case 1:
+            f.instrucoes();
+            break;
+        case 2:
+            f.configurarBanco();
+            break;
+        case 3:
+            f.criarSenha();
+            break;
+        case 4:
+            f.salvarSenhaExistente();
+            break;
+        case 5:
+            f.pegarSenha();
+            break;
+        case 6:
+            f.alterarSenha();
+            break;
+        case 7:
+            f.excluirSenha();
+            break;
+        case 8:
+            f.fazerBackup();
+            break;
+        case 9:
+            f.restaurarBanco();
+            break;
+        default:
+            cout << "Opção inválida" << endl;
+        }
+    }
+    return 0;
+}
